3324b9b122aba8d97b704337074db4197d379cd4
laurensruiz <laurenruiz97@yahoo.com>
2022-05-11 10:31:06 -0400

    validates user id

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,17 +1,50 @@
+const User = require('../users/users-model')
+
 function logger(req, res, next) {
   // DO YOUR MAGIC
+  const timestamp = new Date().toLocaleString()
+  const method = req.method
+  const url = req.originalUrl
+  console.log(`[${timestamp}] ${method} to ${url}`)
+  //console.log('logger middleware')
+  next()
 }
 
-function validateUserId(req, res, next) {
+//this one interacts with database so need it to be async
+async function validateUserId(req, res, next) {
+  try{
+    const user = await User.getById(req.params.id)
+    if(!user){
+      res.status(404).json({
+        message: "user not found"
+      })
+    } else{
+      req.user = user
+      next()
+    }
+  } catch (err) {
+    res.status(500).json({
+        message: "problem finding user"
+      })
+  }
   // DO YOUR MAGIC
+
+  // console.log('validateUserId middleware')
+  // next()
 }
 
 function validateUser(req, res, next) {
   // DO YOUR MAGIC
+  console.log('validateUser middleware')
+  next()
 }
 
 function validatePost(req, res, next) {
   // DO YOUR MAGIC
+  console.log('validatePost middleware')
+  next()
 }
 
 // do not forget to expose these functions to other modules
+
+module.exports = {logger, validateUserId, validatePost, validateUser}
